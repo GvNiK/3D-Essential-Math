3918c8ceedc205083baf1a7213b425d88f5e44ae
Gaurav <gvnik18@gmail.com>
2024-08-08 18:27:09 +0530

    Created Editor Window & Setup Folder Structure
    - Created a floatable(moveable)  Window by the name "Dot Product".
    - Setup Folder Structure for the Project.
    
    [TODO]
    - Continue making the Dot Product Editor.

---
+++ Assets/Code/Scripts/Editor/DotProductEditor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEditor;
+
+public class DotProductEditor : EditorWindow
+{
+    [MenuItem("Tools/Dot Product")]
+    public static void ShowWindow()
+    {
+        DotProductEditor window = (DotProductEditor)GetWindow(typeof(DotProductEditor), true, "Dot Product");
+        window.Show();
+    }
+}
